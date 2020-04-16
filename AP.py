a285e4102cac8810776a909e53f77f04b92a061f
JDVelasquezO <jdvela158@gmail.com>
2020-04-16 14:14:12 -0600

    Set the initial transition

--- AP.py
+++ AP.py
@@ -4,6 +4,7 @@ class AP():
     non_terminals = []
     terminals = []
     non_terminal_initial = ""
+    productions = []
     transitions = []
     
     def __init__(self, name):
@@ -18,8 +19,40 @@ class AP():
     def setNTInitial(self, ntI):
         self.non_terminal_initial = ntI
 
-    def setTransitions(self, transition):
-        self.transitions.append(transition)
+    def setProductions(self, prod):
+        self.productions.append(prod)
+
+    def setTransitions(self):
+        for item in self.productions:
+            
+            transition = { #Por cada produccion se crea una transicion básica
+                "first": {
+                    "from": "",
+                    "readed": "",
+                    "output": ""
+                },
+
+                "last": {
+                    "to": "",
+                    "input": ""
+                },
+
+                "string": ""
+            }
+
+            # Establece transicion para prod. inicial
+            if (item[0] == self.non_terminal_initial):
+                self.setInitialTransition(transition, item[0])
+    
+    def setInitialTransition(self, transition, item):
+        transition["first"]["from"] = "p"
+        transition["first"]["readed"] = "epsilon"
+        transition["first"]["output"] = "epsilon"
+        transition["last"]["to"] = "q"
+        transition["last"]["input"] = item
+        transition["string"] = f"p,epsilon,epsilon;q,{item}"
+        initialTransition = transition
+        self.transitions.append(initialTransition)        
 
     def getNonTerminals(self):
         return self.non_terminals
@@ -29,6 +62,9 @@ class AP():
 
     def getNTInitial(self):
         return self.non_terminal_initial
+
+    def getProductions(self):
+        return self.productions
 
     def getTransitions(self):
         return self.transitions
--- index.py
+++ index.py
@@ -1,14 +1,21 @@
 from AP import AP
 from Stack import Stack
 
-# ap = AP("ap1")
+ap = AP("ap1")
 
-# ap.setNonTerminals("S")
-# ap.setNonTerminals("M")
-# ap.setNonTerminals("N")
+ap.setNonTerminals("S")
+ap.setNonTerminals("M")
+ap.setNonTerminals("N")
 
-# ap.setTerminals("a")
-# ap.setTerminals("b")
-# ap.setTerminals("z")
+ap.setTerminals("x")
+ap.setTerminals("y")
 
-# ap.setNTInitial("S")
+ap.setNTInitial("S")
+
+ap.setProductions("S>xMx")
+ap.setProductions("M>xMx")
+ap.setProductions("M>N")
+ap.setProductions("N>yN")
+ap.setProductions("N>epsilon")
+
+ap.setTransitions()
